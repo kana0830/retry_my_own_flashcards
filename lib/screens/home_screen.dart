3812ea340659437436c78c0d8078fac6e6bdcb70
kf0830s <kf0830s@gmail.com>
2021-11-26 18:40:13 +0900

    Add:ホーム画面[画像/タイトル/ボタン]追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:retry_my_own_flashcards/parts/button_with_icon.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,9 +12,54 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          children: [Image.asset("assets/images/image_title.png")],
+          children: [
+            Expanded(child: Image.asset("assets/images/image_title.png")),
+            titleText(),
+            const Divider(
+              color: Colors.white,
+              height: 30.0,
+              indent: 8.0,
+              endIndent: 8.0,
+            ),
+            ButtonWithIcon(
+              onPressed: () => print("かくにんテスト"),
+              icon: const Icon(Icons.play_arrow),
+              label: "かくにんテストをする",
+              color: Colors.brown,
+            ),
+            ButtonWithIcon(
+              onPressed: () => print("単語一覧"),
+              icon: const Icon(Icons.list),
+              label: "単語一覧を見る",
+              color: Colors.grey,
+            ),
+            const Text(
+              "powered by Kana",
+              style: TextStyle(fontFamily: "Mont"),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget titleText() {
+    return Column(
+      children: const [
+        Text(
+          "私だけの単語帳",
+          style: TextStyle(
+            fontSize: 40.0,
+          ),
+        ),
+        Text(
+          "My Own Flashcards",
+          style: TextStyle(
+            fontSize: 26.0,
+            fontFamily: "Mont",
+          ),
+        ),
+      ],
     );
   }
 }
