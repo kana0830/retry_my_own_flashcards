40bf60f2a87ca013a3924ab31aafb7939f51af76
kf0830s <kf0830s@gmail.com>
2021-11-27 15:55:49 +0900

    Add:編集画面[画面パーツ]追加

--- lib/screens/word_list_screen.dart
+++ lib/screens/word_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'edit_screen.dart';
+
 class WordListScreen extends StatefulWidget {
   @override
   _WordListScreenState createState() => _WordListScreenState();
@@ -8,6 +10,25 @@ class WordListScreen extends StatefulWidget {
 class _WordListScreenState extends State<WordListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("単語一覧"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _addNewWord(),
+        child: const Icon(Icons.add),
+        tooltip: "新しい単語の登録",
+      ),
+    );
+  }
+
+  _addNewWord() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditScreen(),
+      ),
+    );
   }
 }
