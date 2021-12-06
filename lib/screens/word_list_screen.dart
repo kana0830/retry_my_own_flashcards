69c791c66077d83b0b5af14ea1525dc913278ba2
kf0830s <kf0830s@gmail.com>
2021-12-06 19:26:47 +0900

    Add:[一覧画面]一覧表示追加

--- lib/screens/word_list_screen.dart
+++ lib/screens/word_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:retry_my_own_flashcards/db/database.dart';
+import 'package:retry_my_own_flashcards/main.dart';
 
 import 'edit_screen.dart';
 
@@ -8,6 +10,14 @@ class WordListScreen extends StatefulWidget {
 }
 
 class _WordListScreenState extends State<WordListScreen> {
+  List<Word> _wordList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getAllWords();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +30,10 @@ class _WordListScreenState extends State<WordListScreen> {
         child: const Icon(Icons.add),
         tooltip: "新しい単語の登録",
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: _wordListWidget(),
+      ),
     );
   }
 
@@ -28,6 +42,37 @@ class _WordListScreenState extends State<WordListScreen> {
       context,
       MaterialPageRoute(
         builder: (context) => EditScreen(),
+      ),
+    );
+  }
+
+  void _getAllWords() async {
+    _wordList = await database.allWords;
+    setState(() {});
+  }
+
+  Widget _wordListWidget() {
+    return ListView.builder(
+      itemBuilder: (context, int position) => _wordItem(position),
+      itemCount: _wordList.length,
+    );
+  }
+
+  Widget _wordItem(int position) {
+    return Card(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      color: Colors.grey.shade700,
+      child: ListTile(
+        title: Text(
+          _wordList[position].strQuestion,
+        ),
+        subtitle: Text(
+          _wordList[position].strAnswer,
+          style: const TextStyle(fontFamily: "Mont"),
+        ),
       ),
     );
   }
