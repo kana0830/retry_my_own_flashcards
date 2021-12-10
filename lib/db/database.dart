20cb5c4dd38f84445debc1f433093a6a98fb763e
kf0830s <kf0830s@gmail.com>
2021-12-10 18:51:14 +0900

    Edit:コード整形

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -52,11 +52,13 @@ class MyDatabase extends _$MyDatabase {
 
 LazyDatabase _openConnection() {
   // the LazyDatabase util lets us find the right location for the file async.
-  return LazyDatabase(() async {
-    // put the database file, called db.sqlite here, into the documents folder
-    // for your app.
-    final dbFolder = await getApplicationDocumentsDirectory();
-    final file = File(p.join(dbFolder.path, 'words.db'));
-    return NativeDatabase(file);
-  });
+  return LazyDatabase(
+    () async {
+      // put the database file, called db.sqlite here, into the documents folder
+      // for your app.
+      final dbFolder = await getApplicationDocumentsDirectory();
+      final file = File(p.join(dbFolder.path, 'words.db'));
+      return NativeDatabase(file);
+    },
+  );
 }
